81e157ca530162bbb5a7133e4fc069663ab44169
chrinore <chrinoren@gmail.com>
2025-07-27 01:18:18 +0100

    fin de linitialisation start conding

--- backend/controllers/msgControllers.js
+++ backend/controllers/msgControllers.js
@@ -17,16 +17,16 @@ exports.messageController = async (req, res) => {
 
     const newContent = await prisma.message.create({
       data: {
-        content,
-        ip,
-        city,
-        country,
-        provider,
-        browser,
-        os,
-        device,
-        timezone,
-        language,
+        content: content,
+        ip: ip,
+        city: city,
+        country: country,
+        provider: rovider,
+        browser: browser,
+        os: os,
+        device: device,
+        timezone: timezone,
+        language: language,
       },
     });
     res.status(201).json({ message: "message envoyer", newContent });
